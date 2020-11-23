9fa4d39b92079e716c60bbf434d68f04fb780795
Orim Dominic <orimdominic@gmail.com>
2020-11-23 13:36:05 +0100

    penultimate touches

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,56 @@
-// show dropdown on w<767px
-document.getElementById("btnHamburger").addEventListener("click", (ev) => {
-  document.querySelector(".dropdown-header").classList.toggle("d-none");
+function getElem(selector) {
+  return document.querySelector(selector);
+}
+
+function hide(elem) {
+  elem.classList.add("d-none");
+}
+
+function show(elem) {
+  elem.classList.remove("d-none");
+}
+
+function toggleVisibility(elem) {
+  elem.classList.toggle("d-none");
+}
+// toggle mobile dropdown visibility on w<767px when menu btn is clicked
+getElem("#btnHamburger").addEventListener("click", (ev) => {
+  toggleVisibility(getElem(".dropdown-header"));
 });
-// hide dropdown menus on clicking outside dropdown menus
-document.querySelector(".overlay").addEventListener("click", (ev) => {
-  document.querySelector(".dropdown-menu.profile").classList.add("d-none");
-  document.querySelector(".dropdown-menu.new-items").classList.add("d-none");
-  document.querySelector(".overlay").classList.add("d-none");
+
+// hide dropdown menus on clicking outside dropdown menus when any menu is open
+getElem(".overlay").addEventListener("click", (ev) => {
+  hide(getElem(".dropdown-menu.profile"));
+  hide(getElem(".dropdown-menu.new-items"));
+  hide(getElem(".overlay"));
 });
-//
-document
-  .querySelector(".new-items-svg-wrapper")
-  .addEventListener("click", (ev) => {
-    document
-      .querySelector(".dropdown-menu.new-items")
-      .classList.remove("d-none");
-    document.querySelector(".overlay").classList.remove("d-none");
-  });
-//
-document
-  .querySelector(".header-profile-icon")
-  .addEventListener("click", (ev) => {
-    document.querySelector(".dropdown-menu.profile").classList.remove("d-none");
-    document.querySelector(".overlay").classList.remove("d-none");
-  });
-document
-  .querySelector(".dropdown-menu.new-items")
-  .addEventListener("click", (ev) => {
-    document.querySelector(".dropdown-menu.new-items").classList.add("d-none");
-    document.querySelector(".overlay").classList.add("d-none");
-  });
-document
-  .querySelector(".dropdown-menu.profile")
-  .addEventListener("click", (ev) => {
-    document.querySelector(".dropdown-menu.profile").classList.add("d-none");
-    document.querySelector(".overlay").classList.add("d-none");
-  });
+
+// show dropdown menus on clicking top-right svg icons
+getElem(".new-items-svg-wrapper").addEventListener("click", (ev) => {
+  show(getElem(".dropdown-menu.new-items"));
+  show(getElem(".overlay"));
+});
+
+getElem(".header-profile-icon").addEventListener("click", (ev) => {
+  show(getElem(".dropdown-menu.profile"));
+  show(getElem(".overlay"));
+});
+
+// hide dropdown menus on clicking top-right svg icons
+getElem(".dropdown-menu.new-items").addEventListener("click", (ev) => {
+  hide(getElem(".dropdown-menu.new-items"));
+  hide(getElem(".overlay"));
+});
+
+getElem(".dropdown-menu.profile").addEventListener("click", (ev) => {
+  hide(getElem(".dropdown-menu.profile"));
+  hide(getElem(".overlay"));
+});
+
+// toggle top-left profile details visibility on scroll
 document.addEventListener("scroll", (ev) => {
-  const fullNameEl = document.querySelector(".profile__basic .full-name");
-  const tempProfile = document.querySelector(".temp-profile");
+  const fullNameEl = getElem(".profile__basic .full-name");
+  const tempProfile = getElem(".temp-profile");
   if (fullNameEl.getBoundingClientRect().top <= 0) {
     tempProfile.classList.remove("opacity-0");
   } else {
@@ -46,7 +58,7 @@ document.addEventListener("scroll", (ev) => {
   }
 });
 
-const gqlString = `{
+const gqlQuery = `{
         user(login: "sudo-kaizen"){
           avatarUrl
           bio
@@ -58,7 +70,6 @@ const gqlString = `{
               node{
                 description
                 forkCount
-                isFork
                 name
                 primaryLanguage{
                   color
@@ -92,25 +103,18 @@ const gqlString = `{
         }
 `;
 
-// const githubGqlUrl = "https://api.github.com/graphql";
+const githubGqlUrl = "https://api.github.com/graphql";
 
-// fetch(githubGqlUrl, {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     Authorization: `bearer  a1cc775cda82f9e350974ceaf7fe81a4975458d5`,
-//   },
-//   body: JSON.stringify({ query: `${gqlString}` }),
-// })
-//   .then((resp) => resp.json())
-//   .then(console.log)
-//   .catch((err) => {
-//     console.error(err);
-//   });
-
-fetch("http://localhost:3000/data")
+fetch(githubGqlUrl, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `bearer  a1cc775cda82f9e350974ceaf7fe81a4975458d5`,
+  },
+  body: JSON.stringify({ query: `${gqlQuery}` }),
+})
   .then((resp) => resp.json())
-  .then(populateUi)
+  .then(({data}) =>populateUi(data))
   .catch((err) => {
     console.error(err);
   });
@@ -165,17 +169,18 @@ function setStatus({ status: { emojiHTML = null, message = "" } }) {
     const statusEmojiElems = [
       getElem(".avatar-emoji-items .img-emoji"),
       getElem(".profile__status .status-emoji-octocat"),
+      getElem(".emoji-wrapper .emoji-status-img"),
     ];
     statusEmojiElems.forEach((el) => {
       el.src = emojiImgUrl;
     });
   }
+
   const statusTextElems = [
     getElem(".avatar-emoji-items .text-emoji"),
     getElem(".profile__status .status-text"),
-    getElem(".emoji-status-wrapper .emoji-status"),
+    getElem(".emoji-wrapper .emoji-status-text"),
   ];
-
   statusTextElems.forEach((el) => {
     el.innerHTML = message;
   });
@@ -183,6 +188,9 @@ function setStatus({ status: { emojiHTML = null, message = "" } }) {
 
 function setRepoCount({ repositories: { totalCount } }) {
   getElem(".repo-counter").innerHTML = totalCount;
+  getElem(
+    ".repo-filter-message"
+  ).innerHTML = `<strong>${totalCount}</strong> results for <strong>public</strong> repositories`;
 }
 
 function createRepositoriesHTML({ repositories: { edges: reposArr } }) {
@@ -202,194 +210,9 @@ function createRepositoriesHTML({ repositories: { edges: reposArr } }) {
         repositoryTopics: { edges: repoTopics },
       } = repo;
 
-      const createFork = (fork) => {
-        if (fork) {
-          return `
-          <p class="fork-origin">
-            Forked from <a href=${fork.url}>${fork.nameWithOwner}</a>
-          </p>`;
-        }
-        return "";
-      };
-
-      const createDescription = (desc) => {
-        if (desc) {
-          return `<p class="repo-desc">${desc}</p>`;
-        }
-        return "";
-      };
-
-      const createRepoTopics = (topics) => {
-        if (topics.length) {
-          return `
-            <ul class="repo-topics">
-            ${topics
-              .map(({ node }) => {
-                return `<a href="#" class="repo-topic">${node.topic.name}</a>`;
-              })
-              .join("")}
-             </ul>`;
-        }
-        return "";
-      };
-
-      const createRepoLanguage = (lang) => {
-        if (lang) {
-          return `
-<li class="repo-meta__language meta">
-            <span style="background-color: ${lang.color}"  class="lang-colour"></span>
-            <span class="lang-name">${lang.name}</span>
- </li>
-        `;
-        }
-        return "";
-      };
-
-      const createStarHTML = (nStars) => {
-        if (nStars) {
-          return `
-          <li class="repo-meta__language meta">
-                      <span class="star-svg"
-                        ><svg
-                          aria-label="star"
-                          class=""
-                          viewBox="0 0 16 16"
-                          version="1.1"
-                          width="16"
-                          height="16"
-                          role="img"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-                          ></path>
-                        </svg>
-                      </span>
-                      <span class="star-count">${nStars}</span>
-                    </li>`;
-        }
-
-        return "";
-      };
-
-      const createForksHTML = (nForks) => {
-        if (nForks) {
-          return `
-                    <li class="repo-meta__forks meta">
-                      <span class="fork-img">
-                        <svg
-                          aria-label="fork"
-                          class="fork-svg"
-                          viewBox="0 0 16 16"
-                          version="1.1"
-                          width="16"
-                          height="16"
-                          role="img"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                          ></path>
-                        </svg>
-                      </span>
-                      <span class="fork-count">${nForks}</span>
-                    </li>
-    `;
-        }
-        return "";
-      };
-
-      const createStarBtn = (isStarred) => {
-        return `
-             <div class="repo__button-wrapper">
-                  <button class="btn-star-repo">
-                  ${
-                    isStarred
-                      ? `<svg  viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25z"></path></svg>`
-                      : `<svg
-                      aria-label="star"
-                      viewBox="0 0 16 16"
-                      version="1.1"
-                      width="16"
-                      height="16"
-                      role="img"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-                      ></path>
-                    </svg>`
-                  }
-
-                    <span>${isStarred ? "Uns" : "S"}tar</span>
-                  </button>
-                </div> `;
-      };
-
-      const formatDate = (updatedOnStr) => {
-        const getMonth = (index) => {
-          switch (index) {
-            case 0:
-              return "Jan";
-            case 1:
-              return "Feb";
-            case 2:
-              return "Mar";
-            case 3:
-              return "Apr";
-            case 4:
-              return "May";
-            case 5:
-              return "Jun";
-            case 7:
-              return "Jul";
-            case 8:
-              return "Aug";
-            case 9:
-              return "Sep";
-            case 10:
-              return "Oct";
-            case 11:
-              return "Nov";
-            default:
-              return "Dec";
-          }
-        };
-        const dateDiff = (Date.now() - Date.parse(updatedOnStr)) / 1000; // in seconds
-        // console.log(dateDiff);
-        // use 60 seconds as standard to determine date string
-        const dateDiffTime = {
-          secs: dateDiff,
-          mins: dateDiff / 60,
-          hours: dateDiff / 3600,
-          days: dateDiff / 86400,
-          months: dateDiff / (2.592 * Math.pow(10, 6)),
-        };
-        if (dateDiffTime.secs < 60) {
-          return `Updated ${Math.floor(dateDiffTime.secs)} seconds ago`;
-        } else if (dateDiffTime.mins < 60) {
-          // 1 hour
-          return `Updated ${Math.floor(dateDiffTime.mins)} minutes ago`;
-        } else if (dateDiffTime.days < 32) {
-          // 1 month
-          return `Updated ${Math.floor(dateDiffTime.days)} days ago`;
-        } else if (dateDiffTime.months < 12) {
-          // 1 year
-          return `Updated on ${new Date(updatedOnStr).getDate()} ${getMonth(
-            new Date(updatedOnStr).getMonth() + 1
-          )}`;
-        } else {
-          // > 1 year
-          return `Updated on ${new Date(updatedOnStr).getDate()} ${getMonth(
-            new Date(updatedOnStr).getMonth() + 1
-          )} ${new Date(updatedOnStr).getFullYear()}`;
-        }
-        return ``;
-      };
-
       return `
            <li class="repo">
-                <div>
+                <div class="repo__details">
                   <a class="repo-name" href=${url}>${name}</a>
   ${createFork(parent)}
                   ${createDescription(description)}
@@ -411,6 +234,185 @@ function createRepositoriesHTML({ repositories: { edges: reposArr } }) {
     .join("");
 }
 
-function getElem(selector) {
-  return document.querySelector(selector);
+function createFork(fork) {
+  if (fork) {
+    return `
+      <p class="fork-origin">
+        Forked from <a href=${fork.url}>${fork.nameWithOwner}</a>
+      </p>`;
+  }
+  return "";
+}
+
+function createDescription(desc) {
+  if (desc) {
+    return `<p class="repo-desc">${desc}</p>`;
+  }
+  return "";
+}
+
+function createRepoTopics(topics) {
+  if (topics.length) {
+    return `
+      <ul class="repo-topics">
+      ${topics
+        .map(({ node }) => {
+          return `<a href="#" class="repo-topic">${node.topic.name}</a>`;
+        })
+        .join("")}
+      </ul>`;
+  }
+  return "";
+}
+
+function createRepoLanguage(lang) {
+  if (lang) {
+    return `
+      <li class="repo-meta__language meta">
+        <span style="background-color: ${lang.color}"  class="lang-colour"></span>
+        <span class="lang-name">${lang.name}</span>
+      </li>`;
+  }
+  return "";
+}
+
+function createStarHTML(nStars) {
+  if (nStars) {
+    return `
+      <li class="repo-meta__language meta">
+        <span class="star-svg">
+        <svg
+          aria-label="star"
+          class=""
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="16"
+          height="16"
+          role="img">
+            <path
+              fill-rule="evenodd"
+              d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z">
+              </path>
+          </svg>
+        </span>
+        <span class="star-count">${nStars}</span>
+      </li>`;
+  }
+  return "";
+}
+
+function createForksHTML(nForks) {
+  if (nForks) {
+    return `
+      <li class="repo-meta__forks meta">
+        <span class="fork-img">
+          <svg
+            aria-label="fork"
+            class="fork-svg"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            height="16"
+            role="img"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
+            ></path>
+          </svg>
+        </span>
+        <span class="fork-count">${nForks}</span>
+      </li>`;
+  }
+  return "";
+}
+
+function createStarBtn(isStarred) {
+  return `
+    <div class="repo__button-wrapper">
+      <button class="btn-star-repo">
+      ${
+        isStarred
+          ? `<svg  viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25z"></path></svg>`
+          : `<svg
+          aria-label="star"
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="16"
+          height="16"
+          role="img"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
+          ></path>
+        </svg>`
+      }
+        <span>${isStarred ? "Uns" : "S"}tar</span>
+      </button>
+    </div> `;
+}
+
+function formatDate(updatedOnStr) {
+  const dateDiff = (Date.now() - Date.parse(updatedOnStr)) / 1000; // in seconds
+  // use seconds as standard to determine date string for markup
+  const dateDiffTime = {
+    secs: dateDiff,
+    mins: dateDiff / 60,
+    hours: dateDiff / 3600,
+    days: dateDiff / 86400,
+    months: dateDiff / (2.592 * Math.pow(10, 6)),
+  };
+  if (dateDiffTime.secs < 60) {
+    //within 1 min
+    return `Updated ${Math.floor(dateDiffTime.secs)} seconds ago`;
+  } else if (dateDiffTime.mins <= 60) {
+    //within 1 hour
+    return `Updated ${Math.ceil(dateDiffTime.mins)} minutes ago`;
+  } else if (dateDiffTime.hours < 24) {
+    //within 1 hour
+    return `Updated ${Math.ceil(dateDiffTime.hours)} hours ago`;
+  } else if (dateDiffTime.days <= 31) {
+    // within 1 month
+    return `Updated ${Math.floor(dateDiffTime.days)} days ago`; // within 1 year
+  } else if (dateDiffTime.months < 12) {
+    // within 1 year
+    return `Updated on ${new Date(updatedOnStr).getDate()} ${getMonth(
+      new Date(updatedOnStr).getMonth() + 1
+    )}`;
+  } else {
+    // > 1 year
+    return `Updated on ${new Date(updatedOnStr).getDate()} ${getMonth(
+      new Date(updatedOnStr).getMonth() + 1
+    )} ${new Date(updatedOnStr).getFullYear()}`;
+  }
+}
+
+function getMonth(index) {
+  switch (index) {
+    case 0:
+      return "Jan";
+    case 1:
+      return "Feb";
+    case 2:
+      return "Mar";
+    case 3:
+      return "Apr";
+    case 4:
+      return "May";
+    case 5:
+      return "Jun";
+    case 7:
+      return "Jul";
+    case 8:
+      return "Aug";
+    case 9:
+      return "Sep";
+    case 10:
+      return "Oct";
+    case 11:
+      return "Nov";
+    default:
+      return "Dec";
+  }
 }
